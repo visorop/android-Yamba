414ad057510ce4cdea13603d0d40431d2e0918d5
Veselin Demirev <visorop@gmail.com>
2015-07-08 17:24:29 +0300

    Add new DetailsFragmen, DetailsActivity and new layout for actovity main.

--- app/src/main/java/com/marakana/yamba/yamba/TimelineFragment.java
+++ app/src/main/java/com/marakana/yamba/yamba/TimelineFragment.java
@@ -3,11 +3,16 @@ package com.marakana.yamba.yamba;
 import android.app.ListFragment;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 
 public class TimelineFragment extends ListFragment implements
@@ -21,11 +26,26 @@ public class TimelineFragment extends ListFragment implements
     private SimpleCursorAdapter mAdapter; //
     private static final int LOADER_ID = 42;
 
+    private class TimelineViewBinder implements SimpleCursorAdapter.ViewBinder {
+        @Override
+        public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+            if (view.getId() != R.id.list_item_text_created_at) //
+                return false;
+            // Convert timestamp to relative time
+            long timestamp = cursor.getLong(columnIndex); //
+            CharSequence relativeTime = DateUtils
+                    .getRelativeTimeSpanString(timestamp); //
+            ((TextView) view).setText(relativeTime); //
+            return true;
+        }
+    }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) { //
         super.onActivityCreated(savedInstanceState);
         mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item,
                 null, FROM, TO, 0); //
+        mAdapter.setViewBinder(new TimelineViewBinder());
         setListAdapter(mAdapter); //
         getLoaderManager().initLoader(LOADER_ID, null, this);
     }
@@ -49,4 +69,21 @@ public class TimelineFragment extends ListFragment implements
     public void onLoaderReset(Loader<Cursor> loader) { //
         mAdapter.swapCursor(null);
     }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+//
+// Get the details fragment
+        DetailsFragment fragment = (DetailsFragment) getFragmentManager()
+                .findFragmentById(R.id.fragment_details); //
+// Is details fragment visible?
+        if (fragment != null && fragment.isVisible()) { //
+            fragment.updateView(id); //
+        } else {
+            startActivity(new Intent(getActivity(), DetailsActivity.class)
+                    .putExtra(StatusContract.Column.ID, id)); //
+        }
+    }
+
+
 }
